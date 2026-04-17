3710b3edf222549e7cf877f1522c1dead11931f6
hdias78-png <hdias78@gmail.com>
2026-04-17 22:31:48 +0200

    Update matches.js

--- netlify/functions/matches.js
+++ netlify/functions/matches.js
@@ -1,13 +1,15 @@
-const fetch = require("node-fetch");
 exports.handler = async (event) => {
-  const path = event.queryStringParameters.path || "matches";
+  const path = event.queryStringParameters?.path || "matches";
   const r = await fetch(`https://api.football-data.org/v4/${path}`, {
     headers: { "X-Auth-Token": "04b25e0b04144451abb6e47aed171ea4" }
   });
   const data = await r.json();
   return {
-    statusCode: 200,
-    headers: { "Access-Control-Allow-Origin": "*" },
+    statusCode: r.status,
+    headers: { 
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify(data)
   };
 };
